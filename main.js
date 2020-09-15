4b9546b4cde4c19d4814db2e6858df432f586456
Rebecca Eilering <rebeccaeilering@gmail.com>
2020-09-14 22:09:59 -0500

    adding more stats and just some syntax refactoring

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function init() {
     key: 'https://docs.google.com/spreadsheets/d/1nov8X6QtzfL_Z-8fs1Z7VX4VRmJb_AOXpUrakutYfWo/pubhtml',
     callback: function(data) {
       data.forEach(function (item) {
-        document.querySelector(".grid-wrapper").innerHTML += `<article><h3>${item.title}</h3><div>Author: ${item.author}</div><div>Type of book: ${item.type}</div><div>Did I like it: <span class="rate">${item.rate}</span></div></article>`;
+        document.querySelector(".grid-wrapper").innerHTML += `<article><h3>${item.title}</h3><div>Author: ${item.author}</div><div>Type of book: <span class="type">${item.type}</span></div><div>Genre: ${item.genre}</div><div>Did I like it: <span class="rate">${item.rate}</span></div></article>`;
       });
       document.getElementById('count').innerHTML += ' <span>' + data.length + '</span> books read';
 
@@ -35,6 +35,66 @@ function init() {
         }
       });
 
+      const Types = document.querySelectorAll('.type');
+      
+      Types.forEach(function(type, i) {
+        if (type.innerText === 'fiction') {
+          type.closest('article').classList.add('type-fiction');
+        } else if (type.innerText === 'non-fiction') {
+          type.closest('article').classList.add('type-non-fiction');
+        } else if(type.innerText === 'essays') {
+          type.closest('article').classList.add('type-essay');
+        }
+      });
+
+      const bookType = document.querySelector('.book-type');
+
+      function typeFiction() {
+        const fiction = document.querySelectorAll('.type-fiction');    
+        const el = document.createElement('div');
+        el.setAttribute('class', 'fiction');
+        el.textContent = `Fiction: ${fiction.length}`;
+        bookType.appendChild(el);
+      }
+      typeFiction();
+
+      function typeNonFiction() {
+        const nonFiction = document.querySelectorAll('.type-non-fiction');    
+        const el = document.createElement('div');
+        el.setAttribute('class', 'non-fiction');
+        el.textContent = `Non-Fiction: ${nonFiction.length}`;
+        bookType.appendChild(el);
+      }
+      typeNonFiction();
+
+      function typeEssay() {
+        const essay = document.querySelectorAll('.type-essay');    
+        const el = document.createElement('div');
+        el.setAttribute('class', 'essay');
+        el.textContent = `Essay: ${essay.length}`;
+        bookType.appendChild(el);
+      }
+      typeEssay();
+
+      const bookRating = document.querySelector('.book-rating');
+
+      function likedBook() {
+        const thumbUp = document.querySelectorAll('.thumbup');    
+        const el = document.createElement('div');
+        el.setAttribute('class', 'like');
+        el.textContent = `Like: ${thumbUp.length}`;
+        bookRating.appendChild(el);
+      }
+      likedBook();
+
+      function dislikedBook() {
+        const thumbDown = document.querySelectorAll('.thumbdown');   
+        const el = document.createElement('div');
+        el.setAttribute('class', 'dislike');
+        el.textContent = `Dislike: ${thumbDown.length}`;
+        bookRating.appendChild(el);
+      }
+      dislikedBook()
     },
     simpleSheet: true 
   })
