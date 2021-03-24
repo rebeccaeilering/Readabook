2e23b23e9f99c1871b03a91092492af9f4d60b55
Rebecca Eilering <rebeccaeilering@gmail.com>
2021-03-23 22:00:34 -0500

    move stats into modal

--- main.js
+++ main.js
@@ -152,3 +152,29 @@ function init() {
  }
 
 window.addEventListener('DOMContentLoaded', init);
+
+// Get the modal
+const modal = document.getElementById("statsModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("openStats");
+
+// Get the <span> element that closes the modal
+const close = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
